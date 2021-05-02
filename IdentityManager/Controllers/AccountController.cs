a74102c03481f98b1f6a99757b4fb9ceb5e5d1d5
Ahmet Murat Ilgun <muratilgun34@hotmail.com>
2021-05-02 21:41:22 +0300

    Register için gerekli düzenlemeler yapıldı

---
+++ IdentityManager/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityManager.Models;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
+    }
+}
