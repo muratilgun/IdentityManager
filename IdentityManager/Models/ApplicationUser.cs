c723b8c141197b2562a1ff1d3467dfc8df072e59
Ahmet Murat Ilgun <muratilgun34@hotmail.com>
2021-05-02 21:08:52 +0300

    AspNetUser Tablosuna Name column eklendi daha sonradan eklenmen kolonları ayırmak adına Discriminator adında bir kolon daha ekleniyor

---
+++ IdentityManager/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityManager.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required] 
+        public string Name { get; set; }
+
+    }
+}
