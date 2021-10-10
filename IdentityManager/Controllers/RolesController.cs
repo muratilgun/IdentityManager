78dd9a7479a5f9eb665692e308a1858199fcc017
muratilgun <muratilgun34@gmail.com>
2021-10-11 02:33:19 +0300

    role assignments & added toastr

---
+++ IdentityManager/Controllers/RolesController.cs
@@ -0,0 +1,98 @@
+﻿using IdentityManager.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(UserManager<IdentityUser> userManager, ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+        [HttpGet]
+        public IActionResult Upsert(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return View();
+            }
+            else
+            {
+                var objFromDb = _db.Roles.FirstOrDefault(u => u.Id == id);
+                return View(objFromDb);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(IdentityRole roleObje)
+        {
+            if (await _roleManager.RoleExistsAsync(roleObje.Name))
+            {
+                TempData[SD.Error] = "Role already exists.";
+                return RedirectToAction(nameof(Index));
+
+            }
+            if (string.IsNullOrEmpty(roleObje.Id))
+            {
+                await _roleManager.CreateAsync(new IdentityRole() { Name = roleObje.Name });
+                TempData[SD.Success] = "Role created succesfully";
+
+            }
+            else
+            {
+                var objRoleFromDb = _db.Roles.FirstOrDefault(u => u.Id == roleObje.Id);
+                if (objRoleFromDb == null)
+                {
+                TempData[SD.Error] = "Role not found.";
+                    return RedirectToAction(nameof(Index));
+                }
+                objRoleFromDb.Name = roleObje.Name;
+                objRoleFromDb.NormalizedName = roleObje.Name.ToUpper();
+                var result = await _roleManager.UpdateAsync(objRoleFromDb);
+                TempData[SD.Success] = "Role updated succesfully";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var objFromDb = _db.Roles.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                TempData[SD.Error] = "Role not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            var userRolesForThisRole = _db.UserRoles.Where(u => u.RoleId == id).Count();
+            if (userRolesForThisRole > 0)
+            {
+                TempData[SD.Error] = "Cannot delete this role, since there are users assigned to this role.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            await _roleManager.DeleteAsync(objFromDb);
+            TempData[SD.Success] = "Role deleted succesfully";
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ IdentityManager/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using IdentityManager.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var userList = _db.ApplicationUser.ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in userList)
+            {
+                var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+                if (role == null)
+                {
+                    user.Role = "None";
+                }
+                else
+                {
+                    user.Role = roles.FirstOrDefault(us => us.Id == role.RoleId).Name;
+                }
+            }
+            return View(userList);
+
+        }
+    }
+}
